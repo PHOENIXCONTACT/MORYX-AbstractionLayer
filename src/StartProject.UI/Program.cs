4bdfa3f406fd1c66c7984827caaa1511c7828ea3
Jason Becker <jason.becker@phoenixcontact.com>
2021-09-17 13:43:50 +0200

    Preventing a View ramaining displayed although the relevant permission is missing

--- src/StartProject.UI/Program.cs
+++ src/StartProject.UI/Program.cs
@@ -2,7 +2,7 @@
 using System.Configuration;
 using System.IdentityModel.Configuration;
 using System.IdentityModel.Services;
-
+using System.Security.Claims;
 using Moryx.ClientFramework.Kernel;
 
 namespace StartProject.UI
@@ -11,28 +11,33 @@ namespace StartProject.UI
     {
         [STAThread]
         static void Main(string[] args)
-        {
-            var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
-            var sectionName = "system.identityModel";
-            try
-            {
-                if (config.Sections.Get(sectionName) == null)
-                {
-                    config.Sections.Add(sectionName, new SystemIdentityModelSection());
-                    config.Save();
-                    ConfigurationManager.RefreshSection(sectionName);
-                }
-
-                // FederatedAuthentication.FederationConfiguration.IdentityConfiguration.ClaimsAuthorizationManager = new MoryxAuthorizationManager();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Error during authorization preparation. Exception: " + e.Message);
-            }
-
+        {          
             var heartOfLead = new HeartOfLead(args);
+            heartOfLead.AuhtorizeEverything();
             heartOfLead.Initialize();
             heartOfLead.Start();
+        }
+    }
+}
+
+public static class HeartOfLeadExtension
+{
+    public static void AuhtorizeEverything(this HeartOfLead hol)
+    {
+        var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+        var sectionName = "system.identityModel";
+        try
+        {
+            if (config.Sections.Get(sectionName) == null)
+            {
+                config.Sections.Add(sectionName, new SystemIdentityModelSection());
+                config.Save();
+                ConfigurationManager.RefreshSection(sectionName);
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error during authorization preparation. Exception: " + e.Message);
         }
     }
 }
