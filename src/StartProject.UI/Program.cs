2a107d7f4aa821394280a3ab2c605097483534d5
Jason Becker <jason.becker@phoenixcontact.com>
2021-09-22 13:25:34 +0200

    Use the AuthorizeEverything method from ClientFramework

--- src/StartProject.UI/Program.cs
+++ src/StartProject.UI/Program.cs
@@ -13,31 +13,9 @@ namespace StartProject.UI
         static void Main(string[] args)
         {          
             var heartOfLead = new HeartOfLead(args);
-            heartOfLead.AuhtorizeEverything();
+            heartOfLead.AuthorizeEverything();
             heartOfLead.Initialize();
             heartOfLead.Start();
-        }
-    }
-}
-
-public static class HeartOfLeadExtension
-{
-    public static void AuhtorizeEverything(this HeartOfLead hol)
-    {
-        var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
-        var sectionName = "system.identityModel";
-        try
-        {
-            if (config.Sections.Get(sectionName) == null)
-            {
-                config.Sections.Add(sectionName, new SystemIdentityModelSection());
-                config.Save();
-                ConfigurationManager.RefreshSection(sectionName);
-            }
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine("Error during authorization preparation. Exception: " + e.Message);
         }
     }
 }
