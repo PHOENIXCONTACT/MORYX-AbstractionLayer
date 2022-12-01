a2955f62445d78916b92689c0c3c1015e0ab9927
Marisa Goergen <mgoergen@phoenixcontact.com>
2022-12-01 08:44:52 +0100

    Add authorization to endpoints

--- src/StartProject.Asp/Startup.cs
+++ src/StartProject.Asp/Startup.cs
@@ -4,6 +4,9 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Authorization;
+using Moryx.Identity;
+using Microsoft.AspNetCore.Connections;
 
 namespace StartProject.Asp
 {
@@ -21,6 +24,10 @@ namespace StartProject.Asp
             {
                 c.CustomOperationIds(api => ((ControllerActionDescriptor)api.ActionDescriptor).MethodInfo.Name);
             });
+
+            //Authentification
+            services.AddAuthentication(MoryxIdentityDefaults.AuthenticationScheme).AddMoryxIdentity();
+            services.AddSingleton<IAuthorizationPolicyProvider, MoryxAuthorizationPolicyProvider>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,8 +49,11 @@ namespace StartProject.Asp
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
+                var conventionBuilder = endpoints.MapControllers();
                 endpoints.MapRazorPages();
+                //In order to deactivate the IAM
+                //if (!env.IsDevelopment())
+                //    conventionBuilder.WithMetadata(new AllowAnonymousAttribute());
             });
         }
     }
